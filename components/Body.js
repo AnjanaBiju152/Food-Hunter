2b5615bb11c68d7c962b8e76b7c0a80ae5dc16b9
AnjanaBiju152 <anjana.biju200@gmail.com>
2024-07-12 10:23:43 +0530

    changes

--- components/Body.js
+++ components/Body.js
@@ -1,56 +1,80 @@
 import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
-import restaurantList  from "../utilis/mockData";
+import restaurantList from "../utilis/mockData";
 import Shimmer from "./Shimmer";
 const Body = () => {
-    const [ListOfRestaurants,setListOfRestaurant] = useState(restaurantList);
+  const [ListOfRestaurants, setListOfRestaurant] = useState(restaurantList);
+  const [filteredRestaurant, setFilteredRestaurant] = useState(restaurantList);
+
+  const [searchText, setSearchText] = useState("");
+
+  console.log("Body Rendering");
 
   useEffect(() => {
     fetchData();
-}, []);
+  }, []);
 
-const fetchData = async () => {
-  const data = await fetch("https://www.google-analytics.com/j/collect?v=1&_v=j101&a=582924952&t=pageview&_s=1&dl=https%3A%2F%2Fwww.swiggy.com%2Fcity%2Fkolkata&dr=https%3A%2F%2Fin.search.yahoo.com%2F&ul=en-us&de=UTF-8&dt=Order%20Food%20Online%20In%20Kolkata%20%7C%20Food%20Home%20Delivery%20%7C%20Swiggy&sd=24-bit&sr=110x464&vp=110x464&je=0&_u=QACAAEABAAADACAAI~&jid=1230632891&gjid=516490218&cid=698749712.1719807331&tid=UA-53591212-9&_gid=1521183251.1720506086&_r=1&_slc=1&gtm=45He4730n81WGCN3LHv77032815za200&cd1=unsupported&cd2=-&cd3=-&cd4=-&gcd=13l3l3l3l1&dma=0&tag_exp=0&z=1748455537"
- 
-  );
-
-const json = await data.json();
-
- console.log(json);
-
- setListOfRestaurant(json?.data?.cards[2]?.data?.data?.cards);
-};
-  if (ListOfRestaurants.length== 0) {
-      return <Shimmer/>;
-
-  }
-    return (
-    <div  className="body">  
-     <div className="filter">
-         <button 
-        className ="filter-btn"
-        onClick={()  => {
-const filterList = ListOfRestaurants.filter(
- (res) => res.data.avgRating > 4
-
-);
-setListOfRestaurant(filterList);
-        }
-         }
-        > Top rated Restaurants
-        
-            </button>
-    </div>
-
-    <div className="res-container">
-     {ListOfRestaurants.map((restaurant) => (
-      <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
-     ))}
-    </div>
-    
-    </div>
-
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.google-analytics.com/j/collect?v=1&_v=j101&a=582924952&t=pageview&_s=1&dl=https%3A%2F%2Fwww.swiggy.com%2Fcity%2Fkolkata&dr=https%3A%2F%2Fin.search.yahoo.com%2F&ul=en-us&de=UTF-8&dt=Order%20Food%20Online%20In%20Kolkata%20%7C%20Food%20Home%20Delivery%20%7C%20Swiggy&sd=24-bit&sr=110x464&vp=110x464&je=0&_u=QACAAEABAAADACAAI~&jid=1230632891&gjid=516490218&cid=698749712.1719807331&tid=UA-53591212-9&_gid=1521183251.1720506086&_r=1&_slc=1&gtm=45He4730n81WGCN3LHv77032815za200&cd1=unsupported&cd2=-&cd3=-&cd4=-&gcd=13l3l3l3l1&dma=0&tag_exp=0&z=1748455537"
     );
+
+    const json = await data.json();
+
+    console.log(json);
+
+    setListOfRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+    setFilteredRestaurant(json?.data?.cards[2]?.data?.data?.cards);
+  };
+
+  return ListOfRestaurants.length == 0 ? (
+    <Shimmer />
+  ) : (
+    <div className="body">
+      <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              console.log(searchText);
+              const filteredRestaurant = ListOfRestaurants.filter((res) =>
+                res.data.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+
+              setListOfRestaurant(filteredRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = ListOfRestaurants.filter(
+              (res) => res.data.avgRating > 4
+            );
+            setListOfRestaurant(filteredList);
+          }}
+        >
+          {" "}
+          Top rated Restaurants
+        </button>
+      </div>
+
+      <div className="res-container">
+        {ListOfRestaurants.map((restaurant) => (
+          <RestaurantCard key={restaurant.data.id} resData={restaurant} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
- export default Body;
+export default Body;
--- components/Header.js
+++ components/Header.js
@@ -1,28 +1,36 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utilis/constants";
+import { useState } from "react";
 
 const Header = () => {
-    return (
-      <div className ="header">
-          
-          <div className="logo-container">
-                  <img 
-                  className={"logo" }
-                  src={LOGO_URL}/>
-               </div>
-              <div className="nav-items">
-                  <ul>
-                      <li>Home</li>
-                      <li>about Us</li>
-                      <li>Contact Us</li>
-                      <li>Cart</li>
-                  </ul>
-              </div>
-       
-          </div>
-  
+  const [btnNameReact, setBtnNameReact] = useState("Login");
+  console.log("Header render");
 
-    );
-  };
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className={"logo"} src={LOGO_URL} />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>about Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact == "Login"
+                ? setBtnNameReact("Logout")
+                : setBtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-   
-  export default Header;
+export default Header;
